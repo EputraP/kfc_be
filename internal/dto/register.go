2bedbf22d240d49a874edfd8d68b5016e0f434d3
Eka Putra Prasetya <eka.putra@caditglobal.com>
2025-03-13 06:32:24 +0700

    add register dto

---
+++ internal/dto/register.go
@@ -0,0 +1,14 @@
+package dto
+
+import "github.com/google/uuid"
+
+type RegisterBody struct {
+	Username string `json:"username" binding:"required"`
+	Password string `json:"password" binding:"required"`
+	Email    string `json:"email" binding:"required"`
+}
+
+type RegisterResponse struct {
+	UserID   uuid.UUID `json:"user_id"`
+	Username string    `json:"username"`
+}
