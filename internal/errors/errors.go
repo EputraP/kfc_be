82b29b937a4fcc6601d6f1f3b7fda29b610a5d91
Eka Putra Prasetya <eka.putra@caditglobal.com>
2025-03-13 13:50:46 +0700

    add error

--- internal/errors/errors.go
+++ internal/errors/errors.go
@@ -12,11 +12,15 @@ var (
 
 	InvalidRequestBody = errors.New("invalid request body")
 
-	EmailAlreadyUsed          = errors.New("email already used")
-	UsernameAlreadyUsed       = errors.New("username already used")
-	PasswordDoesntMatch       = errors.New("password doesn't match")
-	PasswordContainUsername   = errors.New("password must not contain username")
-	PasswordSameAsBefore      = errors.New("Password cannot be same as before")
-	UsernamePasswordIncorrect = errors.New("username or password incorrect")
-	SearchUsernameError       = errors.New("Error occurred while searching for username")
+	EmailAlreadyUsed           = errors.New("email already used")
+	UsernameAlreadyUsed        = errors.New("username already used")
+	PasswordDoesntMatch        = errors.New("password doesn't match")
+	PasswordContainUsername    = errors.New("password must not contain username")
+	PasswordSameAsBefore       = errors.New("Password cannot be same as before")
+	UsernamePasswordIncorrect  = errors.New("username or password incorrect")
+	SearchUsernameError        = errors.New("Error occurred while searching for username")
+	CheckPasswordError         = errors.New("Error occurred while checking for password")
+	GenerateLoginResponseError = errors.New("Error occurred while generating login response")
+
+	ParseUUIDError = errors.New("Error parsing UUID")
 )
